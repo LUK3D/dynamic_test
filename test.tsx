769dc3d2f2c01323c453ea9cfd43e736b91ecc04
Filipe Lukebana <filipelukebana@gmail.com>
2022-09-30 00:03:33 -0300

    initial commit

---
+++ test.tsx
@@ -0,0 +1,12 @@
+export  const myConmponent = ()=>{
+    return (
+        <>
+        <button style={{
+            padding:'10px 5px',
+            background:'red',
+            color:'white',
+            borderRadius:'10px'
+        }}>Teste Basico Importado</button>
+        </>
+    )
+}
