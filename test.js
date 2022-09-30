364e69f1aa649aef87d0feb2995f3a2296c08eb0
Filipe Lukebana <31851972+LUK3D@users.noreply.github.com>
2022-09-30 00:18:49 -0300

    updates

--- test.js
+++ test.js
@@ -1,12 +1,8 @@
-export  const myConmponent = ()=>{
-    return (
-        <>
+
         <button style={{
             padding:'10px 5px',
             background:'red',
             color:'white',
             borderRadius:'10px'
         }}>Teste Basico Importado</button>
-        </>
-    )
-}
+        
