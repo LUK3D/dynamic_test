a059537c730c695e8e75af3144bdcc7818990816
Filipe Lukebana <31851972+LUK3D@users.noreply.github.com>
2022-09-30 00:19:34 -0300

    Updates

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 
-        <button style={{
-            padding:'10px 5px',
-            background:'red',
-            color:'white',
-            borderRadius:'10px'
-        }}>Teste Basico Importado</button>
+<button style={{
+padding:'10px 5px',
+background:'red',
+color:'white',
+borderRadius:'10px'
+}}>Teste Basico Importado</button>
         
